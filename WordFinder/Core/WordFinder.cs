f307dc93bb793f3e6a20aa581b195739e38f3483
Fernando Oliva <fernando.oliva.hueto@gmail.com>
2014-06-30 10:59:13 +0200

    First Commit

---
+++ WordFinder/Core/WordFinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace WordFinder.Core
+{
+    public static class WordFinder
+    {
+        public static void Search(string loadedFile, string referenceWord)
+        {
+            var text = File.ReadAllText(loadedFile);
+
+            string[] source = text.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == referenceWord.ToLowerInvariant()
+                             select word;
+
+            int wordCount = matchQuery.Count();
+        }
+    }
+}
---
+++ WordFinder/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+using WordFinder.Core;
+
+namespace WordFinder
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog loadFile = new OpenFileDialog();
+
+            loadFile.ShowDialog();
+
+            Core.WordFinder.Search(loadFile.FileName, txtReferenceWord.Text);
+        }
+    }
+}
